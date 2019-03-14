34be3288cea49227d1689853ca1a2e04d398c981
Natalie <natalieschennum@gmail.com>
2019-03-14 14:14:43 -0700

    Recipe Update Route

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,18 +4,12 @@ const router = express.Router();
 //Item Model
 const Item = require('../../models/Item'); 
 
-//@route GET api/items
-//@desc Get ALL items
-// @access Public
 router.get('/', (req, res) => {
     Item.find()
     .sort({ date: -1})
     .then(items => res.json(items));
 })
 
-//@route POST api/items
-//@desc  Create an Item
-// @access Public
 router.post('/', (req, res) => {
    const newItem = new Item({
        name: req.body.name
@@ -23,17 +17,20 @@ router.post('/', (req, res) => {
 
    newItem.save().then(item => res.json(item)); 
 });
-//@route DELETE api/items
-//@desc  Delete an Item
-// @access Public
+
+router.put('/:id', (req, res) => {
+    Item.findByIdAndUpdate(req.params.id, { new: true })
+      .then((newItem) => {
+        res.json(newItem);
+      })
+      .catch(err => res.json(err));
+  });
+
 router.delete('/:id', (req, res) => {
     Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({success: true})))
     .catch(err => res.status(404).json({sucess: false}));
 })
-
-
-
 
 module.exports = router; 
 
