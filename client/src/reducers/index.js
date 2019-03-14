c88cff93f98b886e8c01b51f11961d4ad799daf5
Natalie <natalieschennum@gmail.com>
2019-03-14 14:16:19 -0700

    Recipe Redicer Index

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux'; 
 import itemReducer from './itemReducer'; 
+import recipeReducer from './recipeReducer'; 
+import imageReducer from './imageReducer'; 
 
 export default combineReducers({
-    item: itemReducer
+    item: itemReducer,
+    recipe: recipeReducer,
+    image: imageReducer
 });
