e0ea5333622ca041d9856f02bd5a71a2186a135f
Natalie <natalieschennum@gmail.com>
2019-03-16 13:41:20 -0700

    Reconfig Server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
+const config = require('config');
 const path = require("path");
-const items = require("./routes/api/items");
-const recipes = require("./routes/api/recipes");
 
 // var formidable = require("formidable");
 // var fs = require("fs");
@@ -17,37 +15,36 @@ const app = express();
 
 //Middleware
 app.use(express.json());
-app.use(cors());
+
 
 //DB Config
-const db = require("./config/keys").mongoURI;
+const db = config.get('mongoURI');
 
-mongoose.Promise = global.Promise;
 // Connect to Mongo
 mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB Connected..."))
+  .connect(db, { 
+    useNewUrlParser: true,
+    useCreateIndex: true
+  }) // Adding new mongo url parser
+  .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.use("/items", items);
-app.use("/recipes", recipes);
-// app.use("/image", image, function() {
-//     var img = multer({ inMemory: true });
-//     a.img.data = img.buffer;
-//     a.save(cb);
-//   });
+// Routes
+app.use('/api/items', require('./routes/api/items'));
+app.use('api/recipes', require('/routes/api/recipes'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-//defined as an environmental variable for Heroku
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
